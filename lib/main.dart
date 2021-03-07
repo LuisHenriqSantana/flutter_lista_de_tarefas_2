6471d4941a4752c1ec2f23b8d0b0ec8f8dd90fec
Luis <luis.henrique8000@gmail.com>
2021-03-07 00:33:55 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas_2/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
